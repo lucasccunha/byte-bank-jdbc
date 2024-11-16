355cf693294341091e707327dfdf85ca8afc9bdb
Lucas Cunha <lucas71cunha@gmail.com>
2024-11-16 18:15:01 -0300

    DAO jdbc

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -4,19 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexaoDB {
-    public static void main(String[] args) {
+public class ConnectionFactory {
 
+    public Connection recuperaConexao() {
         try {
-           Connection connection =  DriverManager
+            return DriverManager
                     .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=admin");
-
-            System.out.println("Recuperei a conexão");
-
-            connection.close();
-
         } catch (SQLException e) {
-            System.out.println(e);
+            throw new RuntimeException(e);
         }
     }
 }
